5df92066a72d2113434e9b2f4fc48b4f46899cee
Kimpanzy <kimpersson186@hotmail.com>
2025-04-11 14:28:57 +0200

    flashligth

---
+++ Source/SPM2025/Flashlight.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Flashlight.generated.h"
+
+class USpotLightComponent;
+UCLASS()
+class SPM2025_API AFlashlight : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFlashlight();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	void ToggleFlashlight();
+
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* Root;
+	
+	UPROPERTY(VisibleAnywhere)
+	USpotLightComponent* OuterSpotLight;
+	USpotLightComponent* InnerSpotLight;
+
+	UPROPERTY(VisibleAnywhere)
+	bool bIsOn;
+
+};
