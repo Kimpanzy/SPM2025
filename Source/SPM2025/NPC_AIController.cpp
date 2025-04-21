09cedf32b7511a4b21956135c02a65030a3234d0
Kimpanzy <kimpersson186@hotmail.com>
2025-04-21 22:04:46 +0200

    Revert "AI BT"
    This reverts commit 49d923557a5f30a5c44fe88b65254a9ad9af3710.

--- Source/SPM2025/NPC_AIController.cpp
+++ Source/SPM2025/NPC_AIController.cpp
@@ -83,11 +83,5 @@ void ANPC_AIController::OnTargetDetection(AActor* Actor, FAIStimulus Stimulus)
 	{
 		//GetBlackboardComponent()->SetValueAsBool("CanSeePlayer",Stimulus.WasSuccessfullySensed());
 		GetBlackboardComponent()->SetValueAsBool("CanHearPlayer",Stimulus.WasSuccessfullySensed());
-		GetBlackboardComponent()->SetValueAsObject("TargetActor",player);
-	}
-	else
-	{
-		GetBlackboardComponent()->SetValueAsBool("CanHearActor",Stimulus.WasSuccessfullySensed());
-		GetBlackboardComponent()->SetValueAsVector("TargetLocation",Actor->GetActorLocation());
 	}
 }
