680dcdaac12a3da4c597edcb740b2229ecaf83c3
Olivoz <48181323+Olivoz@users.noreply.github.com>
2025-05-19 16:29:45 +0200

    Update box trigger

--- Source/SPM2025/PushableBallGoal.cpp
+++ Source/SPM2025/PushableBallGoal.cpp
@@ -26,7 +26,7 @@ void APushableBallGoal::OnOverlap(UPrimitiveComponent* OverlappedComponent, AAct
                                   UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
                                   const FHitResult& SweepResult)
 {
-	if (!OtherActor->ActorHasTag(TEXT("Ball")) || !bIsComplete)
+	if (bIsComplete || !OtherActor->ActorHasTag(TEXT("Ball")))
 	{
 		return;
 	}
